b062688941cebae338b65a71fd16221f51e3ab7a
Shaik Pasha <shaikpasha@aeologic.com>
2026-07-27 17:46:45 +0530

    Build responsive layout and home page structure

---
+++ src/app/features/home/components/top-companies/top-companies.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-companies',
+  imports: [],
+  templateUrl: './top-companies.html',
+  styleUrl: './top-companies.css',
+})
+export class TopCompanies {}
