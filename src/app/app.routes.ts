575b6414ab5186e88a22ca9a9fd708d8796de256
Shaik Pasha <shaikpasha@aeologic.com>
2026-07-27 17:41:21 +0530

    Configure routing, models, HttpClient, and JSON Server

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,29 @@
 import { Routes } from '@angular/router';
 
+import { Home } from './features/home/pages/home/home';
+import { Jobs } from './features/jobs/pages/jobs/jobs';
+import { JobDetails } from './features/job-details/pages/job-details/job-details';
+import { SavedJobs } from './features/saved-jobs/pages/saved-jobs/saved-jobs';
+
 export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'jobs',
+    component: Jobs
+  },
+  {
+    path: 'jobs/:id',
+    component: JobDetails
+  },
+  {
+    path: 'saved',
+    component: SavedJobs
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
